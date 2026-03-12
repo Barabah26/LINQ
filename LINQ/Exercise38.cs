3d63e146d612d46c0b1335e8d0583caabccc275e
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-03-12 21:38:59 +0200

    Coding Exercise 39: OrderBy, First & Last

--- LINQ/Exercise38.cs
+++ LINQ/Exercise38.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace LINQ;
 
-namespace LINQ
+public class Exercise38
 {
-    internal class Exercise38
+    public static int CountListsContainingZeroLongerThan(int length, List<List<int>> listsOfNumbers)
     {
+        return listsOfNumbers.Count(n => n.Contains(0) && n.Count() > length);
     }
 }
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿var words = new List<string> { "a", "bb", "ccc", "dddd"};
+var wordsLongerTnan2Letters = words.Where(w => w.Length > 2);
+
+
+var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+var oddNumber = numbers.Where(n => n % 2 == 1);
+
+Console.ReadKey();
