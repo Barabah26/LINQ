e1708657550b293465e3ea9facd33c058e4fff98
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-03-12 20:55:42 +0200

    Coding Exercise 37: Any & All

---
+++ LINQ/Exercise37.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LINQ;
+
+public class Exercise37
+{
+    public static bool IsAnyWordWhiteSpace(List<string> words)
+    {
+        return words != null && words.Any(word => !string.IsNullOrEmpty(word) && word.All(char.IsWhiteSpace));
+    }
+}
