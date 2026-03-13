7776b81fe95a4b8f1077821e66ec5c98faf4df10
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-03-13 08:37:15 +0200

    Coding Exercise 41: Select & Average

---
+++ LINQ/Exercise41.cs
@@ -0,0 +1,9 @@
+﻿namespace LINQ;
+
+public class Exercise41
+{
+    public static double CalculateAverageDurationInMilliseconds(IEnumerable<TimeSpan> timeSpans)
+    {
+        return timeSpans.Select(t => t.Milliseconds).Average();
+    }
+}
